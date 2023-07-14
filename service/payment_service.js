f62c9956bac2e0117eea764a1eda339f63296e8b
sergeynochevkin <sergey.nochevkin@gmail.com>
2023-07-14 12:54:16 +0300

    payment

--- service/payment_service.js
+++ service/payment_service.js
@@ -38,7 +38,7 @@ class PaymentService {
                         description: order_details.description,
                         quantity: order_details.quantity,
                         amount: {
-                            value: invoice.price,
+                            value: 10,
                             currency: "RUB"
                         },
                         vat_code: "1"
