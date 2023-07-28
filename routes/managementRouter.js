99cbc87c87e052161bca3c25ae08eaf6fdaf0a0b
sergeynochevkin <sergey.nochevkin@gmail.com>
2023-07-28 12:15:58 +0300

    auth management

--- routes/managementRouter.js
+++ routes/managementRouter.js
@@ -4,10 +4,10 @@ const managementController = require('../controllers/managementController')
 const auth_middleware = require('../middleware/auth_middleware')
 const checkRoleMiddleware = require('../middleware/checkRoleMiddleware')
 
-router.get('/get_users', checkRoleMiddleware('admin'), managementController.get_users)
-router.get('/get_orders', checkRoleMiddleware('admin'), managementController.get_orders)
-router.get('/get_transports', checkRoleMiddleware('admin'), managementController.get_transports)
-router.post('/send_notification', checkRoleMiddleware('admin'), managementController.send_notification)
-router.put('/', checkRoleMiddleware('admin'), managementController.updateField)
+router.get('/get_users', auth_middleware, managementController.get_users)
+router.get('/get_orders', auth_middleware, managementController.get_orders)
+router.get('/get_transports', auth_middleware, managementController.get_transports)
+router.post('/send_notification', auth_middleware, managementController.send_notification)
+router.put('/', auth_middleware, managementController.updateField)
 
 module.exports = router
