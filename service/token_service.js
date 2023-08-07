8bb0cb6a4de2f2951027ebb0dfa439c7bb71093a
sergeynochevkin <sergey.nochevkin@gmail.com>
2023-08-07 12:50:17 +0300

    token 12h

--- service/token_service.js
+++ service/token_service.js
@@ -4,7 +4,7 @@ const { Token } = require('../models/models')
 class TokenService {
 
     async generateTokens(payload) {
-        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, { expiresIn: '15m' })
+        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, { expiresIn: '12h' })
         const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, { expiresIn: '30d' })
         return {
             accessToken,
